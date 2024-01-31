2336abb3cf913b43bf12cb14fa2982d1b9ee86eb
lucas-10101 <lucas-quadros@outlook.com.br>
2024-01-31 20:40:00 -0300

    Fully configured to use eureka server

--- hr-worker/src/main/java/br/com/fullstackdeveloper/hrworker/controllers/WorkerController.java
+++ hr-worker/src/main/java/br/com/fullstackdeveloper/hrworker/controllers/WorkerController.java
@@ -2,6 +2,8 @@ package br.com.fullstackdeveloper.hrworker.controllers;
 
 import java.util.List;
 
+import javax.persistence.EntityNotFoundException;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,7 +15,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 import br.com.fullstackdeveloper.hrworker.data.entities.Worker;
 import br.com.fullstackdeveloper.hrworker.data.repositories.WorkerRepository;
-import jakarta.persistence.EntityNotFoundException;
 
 @RestController
 @RequestMapping(path = "/workers")
--- hr-worker/src/main/java/br/com/fullstackdeveloper/hrworker/data/entities/Worker.java
+++ hr-worker/src/main/java/br/com/fullstackdeveloper/hrworker/data/entities/Worker.java
@@ -3,12 +3,13 @@ package br.com.fullstackdeveloper.hrworker.data.entities;
 import java.io.Serializable;
 import java.math.BigDecimal;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
 import lombok.Data;
 
 @Data
